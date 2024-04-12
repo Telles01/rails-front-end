62bb3c7d2edb5712e3078cf38dcf7e969b3c7b7f
Bernard Teles <bernardtelgon@gmail.com>
2024-04-12 05:25:26 -0300

    learning assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  #get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-   root "pages#home"
-
-
+  get "pages/about"
+  root "pages#home"
 end
